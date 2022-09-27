2805c35085928820a1c9f0f6581b3f21e6db139a
oleg <poroshin.oa72@gmail.com>
2022-09-27 06:29:10 +0300

    **РЕШЕНО** Задача 2

--- Seminar_9-DZ/task_2/Program.cs
+++ Seminar_9-DZ/task_2/Program.cs
@@ -2,3 +2,28 @@
 
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+// Ввод данных
+int sum = 0;
+int m = InputNumbers("Введите значение M: ");
+int n = InputNumbers("Введите значение N: ");
+
+// Обработка ввода
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+// Расчет суммы
+int rekursion_int(int m, int n){
+    if (m <= n){
+        sum = sum + m;
+        rekursion_int(m + 1, n);
+    }
+
+    return sum;
+}
+
+Console.WriteLine(rekursion_int(m, n));
