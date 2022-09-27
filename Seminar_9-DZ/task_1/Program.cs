e5bb4a1e9eda493614b9f9bbb42bea8e11dc3114
oleg <poroshin.oa72@gmail.com>
2022-09-27 06:04:48 +0300

    **РЕШЕНО** Задача 1

--- Seminar_9-DZ/task_1/Program.cs
+++ Seminar_9-DZ/task_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// Ввод данных
+int n = InputNumbers("Введите значение N: ");
+
+// Обработка ввода
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
+// Вывод чисел
+void rekursion_int(int input_int){
+    if (input_int > 0){
+        Console.WriteLine(input_int);
+        rekursion_int(input_int - 1);
+    }
+}
+
+rekursion_int(n);
